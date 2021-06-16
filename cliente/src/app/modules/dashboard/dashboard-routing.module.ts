e2382bc5f54bd50d4d3186c247e9e94157feb6eb
vivianapatino <44483067+vivianapatino@users.noreply.github.com>
2021-06-16 00:08:06 -0500

    creacion de modulo create-test

--- cliente/src/app/modules/dashboard/dashboard-routing.module.ts
+++ cliente/src/app/modules/dashboard/dashboard-routing.module.ts
@@ -47,6 +47,7 @@ const routes: Routes = [
             (m) => m.AdminModule
           ),
       },
+     
     ]
   }
 ];
--- cliente/src/app/modules/dashboard/test/test-routing.module.ts
+++ cliente/src/app/modules/dashboard/test/test-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateTestComponent } from './create-test/create-test.component';
 import { MainComponent } from './main/main.component';
 import { TestResultComponent } from './test-result/test-result.component';
 import { ViewTestComponent } from './view-test/view-test.component';
@@ -7,7 +8,8 @@ import { ViewTestComponent } from './view-test/view-test.component';
 const routes: Routes = [
   {path: '', component: MainComponent},
   {path: 'result', component: TestResultComponent},
-  {path: ':id', component: ViewTestComponent}
+  {path: ':id', component: ViewTestComponent},
+  {path: '', component: CreateTestComponent}
 ];
 
 @NgModule({
