11b32b084b7e238c254054e84c2f73804952e603
Luis Jaimes <luisalbertoj.tober@gmail.com>
2021-06-17 20:54:03 -0500

    merge

--- cliente/src/app/modules/dashboard/test/view-test/view-test.component.ts
+++ cliente/src/app/modules/dashboard/test/view-test/view-test.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import * as moment from 'moment';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { FactoryService } from 'src/app/services/factory.service';
 import Swal from 'sweetalert2';
@@ -12,6 +13,16 @@ import Swal from 'sweetalert2';
 export class ViewTestComponent implements OnInit {
   public test = null;
   public respuestas = [];
+  public duracion: any = 0;
+  MILLISECONDS_OF_A_SECOND = 1000;
+  MILLISECONDS_OF_A_MINUTE = this.MILLISECONDS_OF_A_SECOND * 60;
+  MILLISECONDS_OF_A_HOUR = this.MILLISECONDS_OF_A_MINUTE * 60;
+  MILLISECONDS_OF_A_DAY = this.MILLISECONDS_OF_A_HOUR * 24;
+  SPAN_DAYS: any = 0;
+  SPAN_HOURS: any = 0;
+  SPAN_MINUTES: any = 0;
+  SPAN_SECONDS: any = 0;
+  DATE_TARGET: any = new Date('06/18/2021 0:01 AM');
 
   constructor(
     public factory: FactoryService,
@@ -36,6 +47,14 @@ export class ViewTestComponent implements OnInit {
         } else {
           this.test = response.data;
           console.log(this.test);
+          const end: any = moment(this.test.cierre, "YYYY/MM/DD HH:mm:ss");
+          const startTime: any = moment(this.test.inicio, "YYYY/MM/DD HH:mm:ss");
+          this.duracion = moment.duration(end.diff(startTime, "YYYY/MM/DD HH:mm:ss"));
+          console.log(this.duracion._milliseconds/ 60000);
+          console.log(this.duracion);
+          console.log(end, startTime);
+          this.updateCountdown();
+          setInterval(this.updateCountdown, this.MILLISECONDS_OF_A_SECOND);
         }
       });
   }
@@ -60,4 +79,22 @@ export class ViewTestComponent implements OnInit {
     localStorage.setItem('result', JSON.stringify(response));
     this.router.navigate(['dashboard/test/result']);
   }
+  calcularTiempo() {
+    
+  }
+  updateCountdown() {
+    // Calcs
+    const NOW: any = new Date()
+    const DURATION = this.DATE_TARGET - NOW;
+    const REMAINING_DAYS = Math.floor(DURATION / this.MILLISECONDS_OF_A_DAY);
+    const REMAINING_HOURS = Math.floor((DURATION % this.MILLISECONDS_OF_A_DAY) / this.MILLISECONDS_OF_A_HOUR);
+    const REMAINING_MINUTES = Math.floor((DURATION % this.MILLISECONDS_OF_A_HOUR) / this.MILLISECONDS_OF_A_MINUTE);
+    const REMAINING_SECONDS = Math.floor((DURATION % this.MILLISECONDS_OF_A_MINUTE) / this.MILLISECONDS_OF_A_SECOND);
+
+    // Render
+    this.SPAN_DAYS = REMAINING_DAYS;
+    this.SPAN_HOURS = REMAINING_HOURS;
+    this.SPAN_MINUTES = REMAINING_MINUTES;
+    this.SPAN_SECONDS = REMAINING_SECONDS;
+}
 }
