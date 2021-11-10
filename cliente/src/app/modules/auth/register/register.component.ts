49d86baee7fced40ba17a0c5f0b74ec17d72d87a
daniapraezss <87150240+daniapraezss@users.noreply.github.com>
2021-11-10 10:55:09 -0500

    forms

--- cliente/src/app/modules/auth/register/register.component.ts
+++ cliente/src/app/modules/auth/register/register.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { NgxSpinnerService } from 'ngx-spinner';
-import { ToastrService } from 'ngx-toastr';
-import { FactoryService } from 'src/app/services/factory.service';
+import { Component, OnInit } from '@angular/core'
+import { Router } from '@angular/router'
+import { NgxSpinnerService } from 'ngx-spinner'
+import { ToastrService } from 'ngx-toastr'
+import { FactoryService } from 'src/app/services/factory.service'
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css'],
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  codigoRegistro: any = '';
-  grupos: any = [];
+  codigoRegistro: any = ''
+  grupos: any = []
 
   public persona: any = {
     nombre: '',
@@ -20,8 +20,8 @@ export class RegisterComponent implements OnInit {
     username: '',
     password: '',
     idRol: '2',
-    grupos: '',
-  };
+    grupos: ''
+  }
 
   constructor(
     private factory: FactoryService,
@@ -31,66 +31,73 @@ export class RegisterComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.factory.loadUser();
-    this.factory.user ? this.router.navigate(['dashboard']) : 0;
+    this.factory.loadUser()
+    this.factory.user ? this.router.navigate(['dashboard']) : 0
     this.factory.returnAsObservable().subscribe((subs) => {
-      subs === true ? this.spinner.hide() : this.spinner.show();
-    });
-    this.loadGrupos();
+      subs === true ? this.spinner.hide() : this.spinner.show()
+    })
+    this.loadGrupos()
+    this.loadRol()
   }
-
+  loadRol() {
+    this.factory
+      .getAll(`rol?where={"nombre": {"contains": "estudiante"}}&populate=false`)
+      .subscribe((arg) => {
+        this.persona.idRol = arg[0].id
+      })
+  }
   loadGrupos(): any {
     this.factory.getAll('grupo').subscribe(
       (response: any) => {
-        this.grupos = response;
+        this.grupos = response
         this.grupos.forEach((grupo) => {
-          grupo.codigoCrypt = this.factory.encryptData(grupo.codigo);
-        });
+          grupo.codigoCrypt = this.factory.encryptData(grupo.codigo)
+        })
 
-        console.log('CODIGO ENCRIPTADO', this.grupos);
+        console.log('CODIGO ENCRIPTADO', this.grupos)
       },
       (error: any) => {
-        this.toast.error(error.message);
-        console.log(error);
+        this.toast.error(error.message)
+        console.log(error)
       }
-    );
+    )
   }
 
   registrar(): any {
-    this.persona.grupos = '';
-    const grupoRegistro = this.factory.decryptData(this.codigoRegistro);
+    this.persona.grupos = ''
+    const grupoRegistro = this.factory.decryptData(this.codigoRegistro)
     this.grupos.forEach((element) => {
       if (grupoRegistro === element.codigo) {
-        (this.persona.grupos = element.id);
+        this.persona.grupos = element.id
       }
-    });
+    })
     if (this.persona.grupos === '') {
-      return this.toast.info('Codigo de registro invalido');
+      return this.toast.info('Codigo de registro invalido')
     }
     if (this.persona.nombre === '') {
-      return this.toast.error('Debes ingresar tu nombre');
+      return this.toast.error('Debes ingresar tu nombre')
     }
     if (this.persona.apellido === '') {
-      return this.toast.error('Debes ingresar tu apellido');
+      return this.toast.error('Debes ingresar tu apellido')
     }
     if (this.persona.email === '') {
-      return this.toast.error('Debes ingresar tu correo electronico');
+      return this.toast.error('Debes ingresar tu correo electronico')
     }
     if (this.persona.username === '') {
-      return this.toast.error('Debes ingresar tu username');
+      return this.toast.error('Debes ingresar tu username')
     }
     if (this.persona.password === '') {
-      return this.toast.error('Debes ingresar tu contraseña');
+      return this.toast.error('Debes ingresar tu contraseña')
     }
-    this.factory.post('persona', this.persona).subscribe(
+    this.factory.post('persona/registrar', this.persona).subscribe(
       (response: any) => {
-        this.toast.success('usuario creado');
-        this.router.navigate(['auth']);
+        this.toast.success('usuario creado')
+        this.router.navigate(['auth'])
       },
       (error: any) => {
-        console.log(error);
-        this.toast.error('Error al registrar el usuario: ' + error.message);
+        console.log(error)
+        this.toast.error('Error al registrar el usuario: ' + error.message)
       }
-    );
+    )
   }
 }
