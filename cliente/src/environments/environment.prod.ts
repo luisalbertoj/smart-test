a9dddc348bdc04a830b4404608015139a816338a
Luis Jaimes <luisalbertoj.tober@gmail.com>
2021-11-23 16:57:34 -0500

    google deploy

--- cliente/src/environments/environment.prod.ts
+++ cliente/src/environments/environment.prod.ts
@@ -3,8 +3,10 @@ export const environment = {
   /* urlApi: 'https://app.tdea.edu.co/smart-test/',
   urlMedia: 'https://app.tdea.edu.co/smart-test/uploads/',
   urlClient: 'https://app.tdea.edu.co/SmartTest/', */
-  urlApi: 'https://smart-test-api.herokuapp.com/',
-  urlMedia: 'https://smart-test-api.herokuapp.com/uploads/',
+  /* urlApi: 'https://smart-test-api.herokuapp.com/',
+  urlMedia: 'https://smart-test-api.herokuapp.com/uploads/', */
+  urlApi: 'http://34.125.115.174:1337/',
+  urlMedia: 'http://34.125.115.174:1337/uploads/',
   urlClient: 'https://app.tdea.edu.co/SmartTest/',
   secretKey: 'UdesTDA'
 };
