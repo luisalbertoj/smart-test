9290c8c0079f71f3fec639f88a58c66a4901fedb
Luis Jaimes <luisalbertoj.tober@gmail.com>
2021-11-24 13:11:03 -0500

    m

--- cliente/src/environments/environment.prod.ts
+++ cliente/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
-  /* urlApi: 'https://app.tdea.edu.co/smart-test/',
-  urlMedia: 'https://app.tdea.edu.co/smart-test/uploads/',
-  urlClient: 'https://app.tdea.edu.co/SmartTest/', */
+  urlApi: 'https://app.tdea.edu.co/smart-test/',
+  urlMedia: 'https://app.tdea.edu.co/smart-test/assets/uploads/',
+  urlClient: 'https://app.tdea.edu.co/SmartTest/',
   /* urlApi: 'https://smart-test-api.herokuapp.com/',
   urlMedia: 'https://smart-test-api.herokuapp.com/uploads/', */
-  urlApi: 'http://34.125.115.174:1337/',
+  /* urlApi: 'http://34.125.115.174:1337/',
   urlMedia: 'http://34.125.115.174:1337/uploads/',
-  urlClient: 'https://app.tdea.edu.co/SmartTest/',
+  urlClient: 'https://app.tdea.edu.co/SmartTest/', */
   secretKey: 'UdesTDA'
 };
