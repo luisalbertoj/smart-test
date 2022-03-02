b7707cec846a2b6e4474f45b128c575cce86618c
Luis Jaimes <luisalbertoj.tober@gmail.com>
2022-03-01 20:52:05 -0500

    fix(backend/model/persona) create integration test model of persona 🧪🧑‍🤝‍🧑
    >issue#2 (iModel Integration test #2)

--- test/integration/models/Persona.test.js
+++ test/integration/models/Persona.test.js
@@ -1,26 +1,90 @@
 var util = require('util');
 
-describe('Persona (model)', () => {
+const newUser = {
+  nombre: 'New User',
+  apellido: 'test',
+  email: 'test@mail.com',
+  cedula: '123456789',
+  codigo: '123456789',
+  password: 'password',
+  username: 'test',
+};
+
+describe('Persona Crud 📑', () => {
   describe('#findAllUsers()', () => {
     it('find users', (done) => {
-      console.log('entro');
       Persona.find()
         .then((users) => {
           if (users.length === 0) {
             return done(
               new Error(
                 'Should return users ' +
-                  'from our test fixtures who are considered the "best".  ' +
-                  'But instead, got: ' +
-                  util.inspect(find, { depth: null }) +
-                  ''
+                util.inspect(find, { depth: null }) +
+                ''
               )
             );
-          } //-•
-
+          }
           return done();
         })
         .catch(done);
     });
   });
+  describe('#CrudUser 🤵', () => {
+    it('add user', (done) => {
+      Persona.create(newUser)
+        .then((user) => {
+          if (!user) {
+            return done(
+              new Error(
+                'Should create user test ' +
+                util.inspect(find, { depth: null }) +
+                ''
+              )
+            );
+          }
+          return done();
+        })
+        .catch(done);
+    });
+    it('FindUser 😃', (done) => {
+      Persona.findOne(newUser.username)
+        .then((user) => {
+          if (!user) {
+            return done(
+              new Error(
+                'Should create user test ' +
+                util.inspect(find, { depth: null }) +
+                ''
+              )
+            );
+          }
+          if (user.id) {
+            newUser.id = user.id;
+          }
+          return done();
+        })
+        .catch(done);
+    });
+    it('DeleteUser 🖲️', (done) => {
+      Persona.destroyOne(newUser.id)
+        .then((isDeleted) => {
+          if (!isDeleted) {
+            return done(
+              new Error(
+                'Should delete user ' +
+                util.inspect(find, { depth: null }) +
+                ''
+              )
+            );
+          }
+          if (user.id) {
+            newUser.id = user.id;
+          }
+          return done();
+        })
+        .catch(done);
+    });
+
+  });
+
 });
